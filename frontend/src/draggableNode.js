831302ccd9e1ec4e30eb04657d67f8033a6aa153
Kuldeep Vansadia <kuldeep@solvei8.com>
2024-09-13 15:58:56 +0530

    improve style

--- frontend/src/draggableNode.js
+++ frontend/src/draggableNode.js
@@ -1,33 +1,47 @@
 // draggableNode.js
 
 export const DraggableNode = ({ type, label }) => {
-    const onDragStart = (event, nodeType) => {
-      const appData = { nodeType }
-      event.target.style.cursor = 'grabbing';
-      event.dataTransfer.setData('application/reactflow', JSON.stringify(appData));
-      event.dataTransfer.effectAllowed = 'move';
-    };
-  
-    return (
-      <div
-        className={type}
-        onDragStart={(event) => onDragStart(event, type)}
-        onDragEnd={(event) => (event.target.style.cursor = 'grab')}
-        style={{ 
-          cursor: 'grab', 
-          minWidth: '80px', 
-          height: '60px',
-          display: 'flex', 
-          alignItems: 'center', 
-          borderRadius: '8px',
-          backgroundColor: '#1C2536',
-          justifyContent: 'center', 
-          flexDirection: 'column'
-        }} 
-        draggable
-      >
-          <span style={{ color: '#fff' }}>{label}</span>
-      </div>
+  const onDragStart = (event, nodeType) => {
+    const appData = { nodeType };
+    event.target.style.cursor = "grabbing";
+    event.dataTransfer.setData(
+      "application/reactflow",
+      JSON.stringify(appData)
     );
+    event.dataTransfer.effectAllowed = "move";
   };
-  
+
+  return (
+    <div
+      className={`draggable-node ${type}`}
+      onDragStart={(event) => onDragStart(event, type)}
+      onDragEnd={(event) => (event.target.style.cursor = "grab")}
+      style={{
+        cursor: "grab",
+        minWidth: "100px",
+        height: "70px",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "column",
+        borderRadius: "10px",
+        backgroundColor: "#1C2536",
+        boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)",
+        transition: "all 0.3s ease",
+      }}
+      draggable
+    >
+      <span
+        style={{
+          color: "#fff",
+          fontSize: "14px",
+          fontWeight: "bold",
+          textAlign: "center",
+          padding: "0 10px",
+        }}
+      >
+        {label}
+      </span>
+    </div>
+  );
+};
